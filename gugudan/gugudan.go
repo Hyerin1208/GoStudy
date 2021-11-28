357563b53ab157551822895b21cb8165ee16618e
Rina <fghjiff8@gmail.com>
2021-11-28 11:41:24 +0900

    구구단

--- gugudan/gugudan.go
+++ gugudan/gugudan.go
@@ -6,12 +6,12 @@ func main() {
 
 	for dan := 1; dan <= 9; dan++ {
 
-		if dan == 5 {
-			continue
-		}
 		fmt.Printf("%d단\n", dan)
 
 		for j := 1; j <= 9; j++ {
+			if j == 5 {
+				continue
+			}
 			fmt.Printf("%d * %d = %d\n", dan, j, dan*j)
 		}
 		}
